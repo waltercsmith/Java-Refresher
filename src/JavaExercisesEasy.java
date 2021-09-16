a061edc10283ac04c4ee16c020192e585b6a78e3
Walter Smith <walter.c.smith01@gmail.com>
2021-09-15 19:12:38 -0500

    Called the static method in the main method which takes two int and provides the sum

--- src/JavaExercisesEasy.java
+++ src/JavaExercisesEasy.java
@@ -1,10 +1,11 @@
 public class JavaExercisesEasy {
 
-    public int sumOfTwo(int i, int j){
+    public static int sumOfTwo(int i, int j){
         return i + j;
     }
+
     public static void main(String[] args) {
 
-
+        System.out.println(sumOfTwo(1,2));;
     }
 }
