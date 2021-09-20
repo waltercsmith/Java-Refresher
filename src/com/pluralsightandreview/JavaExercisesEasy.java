298a47c8a2d91c9d51cbedaca6429aa0a1079533
Walter Smith <walter.c.smith01@gmail.com>
2021-09-20 14:57:08 -0500

    Created a int variable to act as a container to hold the int for the for loop.

--- src/com/pluralsightandreview/JavaExercisesEasy.java
+++ src/com/pluralsightandreview/JavaExercisesEasy.java
@@ -20,20 +20,26 @@ public class JavaExercisesEasy {
 
     public static void main(String[] args) {
 
-        int x = 1;
-        while(x <= 5) {
-            System.out.println("the value of x: " + x);
-            x++;
-        }
+//        int x = 1;
+//        while(x <= 5) {
+//            System.out.println("the value of x: " + x);
+//            x++;
+//        }
+//
+//        for (int y = 1; y <= 5; ++y){
+//            System.out.println("the value of y is: " + y);
+//
+//        }
 
-        for (int y = 1; y <= 5; ++y){
-            System.out.println("the value of y is: " + y);
-
+        int container = 0;
+        for (int z = 4; z >= 1; --z){
+            container = container + z;
+//            System.out.println("the value of z is: " + z);
+//            System.out.println("container = " + container);
         }
+            int totalSumContainer = container;
+        System.out.println(totalSumContainer);
 
-        for (int z = 5; z >= 1; --z){
-            System.out.println("the value of z is: " + z);
-        }
 //        System.out.println(sumOfTwo(1,2));
 
 //        System.out.println(factorial(4));
